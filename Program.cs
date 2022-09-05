05f9eb9254a3aed22a08bdd42552b5fb833fa0b5
Robert Olsen <robert.olsen@halmstad.se>
2022-09-05 08:18:42 +0200

    Kommentarer

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿
 using System;
-using System.Collections.Generic;
+using System.Collections.Generic;   //För att kunna använda generiska klasser
 namespace GenExCSharp
 {
     public class Person //en enkel klass
